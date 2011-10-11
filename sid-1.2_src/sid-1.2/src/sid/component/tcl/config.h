f2867d23ed0edbbbf853de1648bf0b8b31b1c354
Adrian <adritheone@gmail.com>
2011-10-10 19:04:51 -0500

    version 1.2

---
+++ sid-1.2_src/sid-1.2/src/sid/component/tcl/config.h
@@ -0,0 +1,72 @@
+/* config.h.  Generated from config.in by configure.  */
+/* config.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if the X Window System is missing or not being used.  */
+/* #undef X_DISPLAY_MISSING */
+
+/* Define if you have the cygwin_conv_to_full_posix_path function.  */
+/* #undef HAVE_CYGWIN_CONV_TO_FULL_POSIX_PATH */
+
+/* Define if you have the cygwin_conv_to_full_win32_path function.  */
+/* #undef HAVE_CYGWIN_CONV_TO_FULL_WIN32_PATH */
+
+/* Define if you have the opendir function.  */
+#define HAVE_OPENDIR 1
+
+/* Define if you have the <blt.h> header file.  */
+/* #undef HAVE_BLT_H */
+
+/* Define if you have the <dirent.h> header file.  */
+#define HAVE_DIRENT_H 1
+
+/* Define if you have the <sys/cygwin.h> header file.  */
+/* #undef HAVE_SYS_CYGWIN_H */
+
+/* Define if you have the <tcl.h> header file.  */
+#define HAVE_TCL_H 1
+
+/* Define if you have the <tk.h> header file.  */
+#define HAVE_TK_H 1
+
+/* Define if you have the X11 library (-lX11).  */
+#define HAVE_LIBX11 1
+
+/* Define if you have the advapi32 library (-ladvapi32).  */
+/* #undef HAVE_LIBADVAPI32 */
+
+/* Define if you have the comdlg32 library (-lcomdlg32).  */
+/* #undef HAVE_LIBCOMDLG32 */
+
+/* Define if you have the dl library (-ldl).  */
+#define HAVE_LIBDL 1
+
+/* Define if you have the gdi32 library (-lgdi32).  */
+/* #undef HAVE_LIBGDI32 */
+
+/* Define if you have the kernel32 library (-lkernel32).  */
+/* #undef HAVE_LIBKERNEL32 */
+
+/* Define if you have the m library (-lm).  */
+#define HAVE_LIBM 1
+
+/* Define if you have the tcl80 library (-ltcl80).  */
+/* #undef HAVE_LIBTCL80 */
+
+/* Define if you have the tk80 library (-ltk80).  */
+/* #undef HAVE_LIBTK80 */
+
+/* Define if you have the user32 library (-luser32).  */
+/* #undef HAVE_LIBUSER32 */
+
+/* Define if you have the winspool library (-lwinspool).  */
+/* #undef HAVE_LIBWINSPOOL */
+
+/* Name of package */
+#define PACKAGE "sidcomp"
+
+/* Version number of package */
+#define VERSION "0.1"
+
+/* Define if POSIX file names should be mapped to win32 */
+/* #undef SIDTCL_MAP_CYGWIN_NAMES */
+
